677c7f73f7173b18b22ebe4a61c44a8de589b957
WVEdesign <wouter_810209@hotmail.com>
2021-04-12 16:07:16 +0200

    test string Wouter is here

--- RapportModule/Program.cs
+++ RapportModule/Program.cs
@@ -6,7 +6,7 @@ namespace RapportModule
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, this is Wouter here);
         }
     }
 }
