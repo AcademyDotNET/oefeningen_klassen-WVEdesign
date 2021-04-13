b3da23a487786766912c33ca2b33d1973a1a92c0
WVEdesign <wouter_810209@hotmail.com>
2021-04-13 11:48:00 +0200

    opstart...

---
+++ Studentklasse/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studentklasse // Handboek1 pagina 71
+{
+    class Student
+    {
+        public string student;
+
+        public string GetStudent()
+        {
+            return student;
+        }
+
+        public void SetStudent(string value)
+        {
+            student = value;
+        }
+
+        public Student()
+        {
+
+        }
+        public void GeefOverzicht()
+        {
+            Console.WriteLine(value: $"student {GetStudent()}");
+        }
+    }
+}
